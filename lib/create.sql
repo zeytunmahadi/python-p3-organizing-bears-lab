99108aa2468397fd766a3615e9d494c5ab498e54
zeitun <zeytunmahadi718@gmail.com>
2024-08-29 16:38:10 +0300

    Performing CRUD operations on relational databases using SQL

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN
+);
+
---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2, "Rowdy", 10, "M", "black", "intense", 1),
+(3, "Tabitha", 6, "F", "dark brown", "nice", 1),
+(4, "Sergeant Brown", 19, "M", "green", "slimy", 0),
+(5, "Melissa", 13, "F", "dark brown", "goofy", 1),
+(6, "Grinch", 2, "M", "black", "grinchy", 1),
+(7, "Wendy", 6, "F", "blue", "naive", 1),
+(8, NULL, 20, "M", "black", "aggressive", 0);
+
+
